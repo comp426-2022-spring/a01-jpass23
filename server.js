81750dfeefcf4010ec878f24edf53b56cc3a8045
jpass23 <jpass23@live.unc.edu>
2022-04-29 12:51:29 -0400

    commit

--- server.js
+++ server.js
@@ -46,4 +46,3 @@ fs.readFile('./www/index.html', 'utf8' , (err, data) => {
 //   })
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-// server.listen(port, () =>{
